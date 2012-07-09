56c9fa0fbb1bafcd534abf298d1c1713f4cf90a9
brousalis <brousapg@gmail.com>
2012-07-09 16:29:09 -0500

    Refactored

--- category_list.rb
+++ category_list.rb
@@ -5,22 +5,17 @@ module Jekyll
       begin
         categories = context['site']['categories']
         unless categories.nil?
-          s << "<ul>"
-          sorted = {}
           post_count = context['site']['posts'].size.to_i
-          categories.each do |cat|
-            sorted[cat[0]] = cat[1].size
-          end
-          sorted = sorted.sort_by {|k,v| v}.reverse!
-          sorted.each do |k,v|
-            s << "<li><em>#{v}</em><a href=\"/blog/categories/#{k}\">#{k}</a><span style=\"width:#{v * 100 / post_count}%\">bar</span><div class=\"#{k}\"></div></li>"
-          end
-          s << "</ul>"
+          categories.sort_by { |cat, posts| posts.size }
+            .reverse()
+            .each do |cat, posts|
+               s << "<li><em>#{posts.size}</em><a href=\"/blog/categories/#{cat}\">#{cat}</a><span style=\"width:#{posts.size * 100 / post_count}%\">bar</span><div class=\"#{cat}\"></div></li>"
+            end
         end
       rescue => boom
         p boom
       end
-      s.string
+      "<ul>#{s.string}</ul>"
     end
   end
 end
